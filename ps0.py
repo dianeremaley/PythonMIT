6df88eef2e7f13b512653b429d980c082c5655df
Diane Remaley <diane.remaley@gmail.com>
2014-11-21 18:18:54 -0800

    MIT Introduction to Computer Science with Python Using OCW - Itunes Diane Remaley
    	new file:   practice_1.py

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# Problem Set 0 - MIT OCW - 6.00SC - Spring 2011
+# Name: Diane Remaley
+# Collaborators: None
+# Time Spent: :05
+
+
+dob = raw_input("What is your date of birth? \n")
+last_name = raw_input("Enter your last name: \n")
+print dob + " " + last_name
+
+
+
+
+
+
